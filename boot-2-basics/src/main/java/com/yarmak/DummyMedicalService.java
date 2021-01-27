bbc6eedd4d9b5afd7b6fe536b9f97c94ffe7f160
Yevhen Yarmak <eugene.yarmack@gmail.com>
2021-01-27 20:45:16 +0200

    test spring additional config hypothesis

--- boot-2-basics/src/main/java/com/yarmak/DummyMedicalService.java
+++ boot-2-basics/src/main/java/com/yarmak/DummyMedicalService.java
@@ -3,8 +3,10 @@ package com.yarmak;
 import com.yarmak.config.VitalProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.lang.invoke.MethodHandles;
 
 @Service
@@ -13,11 +15,20 @@ public class DummyMedicalService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final VitalProperties vitalProperties;
+    private final Environment env;
 
-    public DummyMedicalService(final VitalProperties vitalProperties) {
+    public DummyMedicalService(final VitalProperties vitalProperties, Environment env) {
         this.vitalProperties = vitalProperties;
+        this.env = env;
         LOGGER.info("Heart rate is: {}", vitalProperties.getHeartRate());
         LOGGER.info("Body temperature is: {}", vitalProperties.getBodyTemperature());
         LOGGER.info("Blood pressure is: {}", vitalProperties.getBloodPressure());
+    }
+
+    @PostConstruct
+    public void printProps() {
+        LOGGER.info("PROPERTIES ---");
+        LOGGER.info("classpath: {}", env.getProperty("cp.key"));
+        LOGGER.info("external: {}", env.getProperty("external.key"));
     }
 }
